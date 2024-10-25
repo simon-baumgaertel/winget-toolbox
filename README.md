b9f15517e4494a0e5d81fe892fc8beb55c0fde9f
baumi <s.baumgaertel@protonmail.com>
2024-10-25 20:20:59 +0200

    :sparkles: scheduled task + readme + license

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # winget-toolbox
- 
+
+Streamline your setup:
+
+This project is a work-in-progress PowerShell helper script designed to simplify your experience with the Winget package manager. It streamlines the setup process, allows you to effortlessly install your favorite applications, and enables auto-updates to keep everything fresh and up to date. Get ready to enhance your productivity with minimal hassle!
+
+## Usage
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/simon-baumgaertel/winget-toolbox.git
+   ```
+
+2. Use the default package list `winget_package.json` or provide your own in a json file like this:
+   ```json
+    [
+        {
+            "Name":  "Software name",
+            "Id":  "Publisher.Software",
+            "Category": "Category"
+        }
+    }
+   ```
+
+3. Run the script in PowerShell (depending on your version used)
+    * PowerShell 7+: `./Setup-WinGet.ps1`
+    * PowerShell <= 5: `powershell -ExecutionPolicy Bypass -File .\Setup-WinGet.ps1` 
+
+
+
+## License 
+
+Distributed under the MIT License. See LICENSE.txt for more information.
--- Setup-WinGet.ps1
+++ Setup-WinGet.ps1
@@ -10,7 +10,6 @@ try {
     Add-AppxPackage -Path "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -ForceApplicationShutdown
 }
 
-# get package list
 $selected_packages = @()
 
 $packages = [PSCustomObject](Get-Content "$PSScriptRoot/winget_packages.json" | ConvertFrom-Json)
@@ -29,4 +28,18 @@ if($selected_packages){
 
     }
     
+}
+
+$title    = 'Enable auto-updates'
+$question = 'Do you want to enable auto-updates on system start up (Creates a scheduled task)?'
+$choices  = '&Yes', '&No'
+
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    $trigger = New-ScheduledTaskTrigger -AtLogOn
+    $action = New-ScheduledTaskAction -Execute "PowerShell" -Argument "winget upgrade --all --silent --accept-package-agreements --accept-source-agreements"
+    Register-ScheduledTask -TaskName "WinGet Auto-updates" -TaskPath "winget-toolbox" -Action $action -Trigger $trigger | Out-Null
+    Write-Output "[+] Created a scheduled task to enable auto updates via winget on system start up"
+} else {
+    Write-Output "[i] Auto-updates not enabled."
 }
